64c901076d90ca0e60273aaa82cbbc8a1d4fda53
Mattias <duartemattias3@gmail.com>
2022-09-13 22:28:38 -0400

    se refactorizo el codigo de la entrada y el GSSP

--- components/Entrada.jsx
+++ components/Entrada.jsx
@@ -3,16 +3,18 @@ import Link from "next/link";
 import styles from "../styles/Entrada.module.css";
 import { formatearFecha } from "../helpers";
 
-const Entrada = ({ entrada }) => {
-  const { id, titulo, resumen, descripcion, published_at, imagen } = entrada
+const Entrada = ({ entrada, id }) => {
+  const {titulo, resumen, publishedAt, descripcion, imagen} = entrada;
+  const imagenBlog = imagen.data.attributes;
+  
   return (
     <article className={styles.entrada}>
-      <Image priority="true" layout="responsive" src={`http://localhost:1337${imagen.url}`} width={800} height={600} alt={`Imagen Blog ${titulo}`} />
+      <Image priority="true" layout="responsive" src={imagenBlog.url} width={800} height={600} alt={`Imagen Blog ${titulo}`} />
       
       <div className={styles.entrada_content}>
         <h3>{titulo}</h3>
       
-        <p className={styles.entrada_fecha}>{formatearFecha(published_at)}</p>
+        <p className={styles.entrada_fecha}>{formatearFecha(publishedAt)}</p>
         <p className={styles.entrada_resumen}>{resumen}</p>
       
         <div className={styles.entrada_enlace}>
